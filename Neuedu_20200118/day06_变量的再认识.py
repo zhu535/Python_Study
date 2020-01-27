21cdc9039411e74111994f27f163a228d5ba7b68
zhu535 <521052232@qq.com>
2020-01-27 21:12:29 +0800

    2020-1-27 21:12:26

---
+++ Neuedu_20200118/day06_变量的再认识.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Time : 2020/1/27 20:29
+# @Author : zhu
+# @FileName: day06_变量的再认识.py
+# @Software: PyCharm
+
+# 可变类型：列表，集合，字典
+
+# 不可变类型：字符串，元组，常量
+
+# 在 python 中，== 是用于比较两者的值，is 是比较两者的地址
